22e63a6ddd9d95d02b511e441acbb1b4ebe06633
cwboyd <cwboyd@users.noreply.github.com>
2021-04-27 18:11:51 -0500

    Remove openssl and curl requirement

--- README.md
+++ README.md
@@ -4,10 +4,6 @@
 
 Shared emmet snippets for use by editors, inter alia, VS code.
 
-## Requirements
-
-You must have openssl and curl installed and in the path.
-
 ## Modifying
 
 Edit 'source.json'.  If the value of a snippet is of type object, that object must contain 2 values: source and template
--- generate.js
+++ generate.js
@@ -1,6 +1,10 @@
 
 const fs = require('fs');
 const child_process = require('child_process');
+const fetch = require('sync-fetch');
+const sha512 = require('js-sha512').sha512;
+const buffer = require('buffer');
+
 let errorRethrower = (error) => { if (!!error) throw(error); }
 
 try
@@ -17,11 +21,15 @@ try
             if (!value.hasOwnProperty('template')) throw(`snippet '${key}' requires field template`);
             if (!value.hasOwnProperty('source')) throw(`snippet '${soure}' requires field template`);
 
-            // Calculate the SRI using openssl.  You must have openssl and curl installed.
-            let command = `curl -s ${value.source} | openssl dgst -sha512 -binary | openssl base64 -A`;
-            let sri = process = child_process
-              .execSync(command, errorRethrower)
-              .toString();
+            // Fetch the resource synchronously
+            let downloadedData = fetch(value.source).buffer();
+
+            // Then, we calcuate a SHA512 hash on the fetch resource.  This returns an array,
+            // so that we can convert it to a Buffer, which has a handle base64 conversion on
+            // it.  All the while, we treat the calculated hash bytes as binary.
+            let sha512_digest = sha512.digest(downloadedData);
+            let sha512_buffer = Buffer.from(sha512_digest, 'binary');
+            let sri = sha512_buffer.toString('base64');
 
             // Expand template, replacing $url with the source and $sri with the hash value.
             let template = value
