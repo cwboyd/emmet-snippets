7b78116ac9341cdec16d905eae10cdfa96a89a10
cwboyd <cwboyd@users.noreply.github.com>
2021-05-05 09:22:21 -0500

    Remove 'js-sha512' and use built-in 'crypto' module.

--- generate.js
+++ generate.js
@@ -1,13 +1,19 @@
 
 const fs = require('fs');
 const fetch = require('sync-fetch');
-const sha512 = require('js-sha512').sha512;
-const buffer = require('buffer');
+const crypto = require('crypto');
 
 let errorRethrower = (error) => { if (!!error) throw(error); }
 
 try
 {
+  if (!crypto || !crypto.getHashes) throw "crypto module is in this version of node!";
+
+  // Check if sha512 is within crypto.getHashes()
+  if (!crypto.getHashes().includes('sha512')) {
+    throw `SHA-512 is unsupported by the crypto module in this version of node.\n\nSupported:\n\n${crypto.getHashes()}`;
+  }
+
   fs.readFile('source.json', function(error,data) {
     errorRethrower(error);
 
@@ -23,12 +29,16 @@ try
             // Fetch the resource synchronously
             let downloadedData = fetch(value.source).buffer();
 
-            // Then, we calcuate a SHA512 hash on the fetch resource.  This returns an array,
-            // so that we can convert it to a Buffer, which has a handle base64 conversion on
-            // it.  All the while, we treat the calculated hash bytes as binary.
-            let sha512_digest = sha512.digest(downloadedData);
-            let sha512_buffer = Buffer.from(sha512_digest, 'binary');
-            let sri = sha512_buffer.toString('base64');
+            // Then, we calcuate a SHA512 hash on the fetched resource (which is a Buffer).
+            // When passing a Buffer into a crypto-module created hash, encoding is assumed to be raw
+            // (and therefore encoding argument is ignored).  Producing a digest on the created
+            // hash ends it, so we have to create a new one each time.  The output of this digest()
+            // is a Buffer, so we call Buffer's special toString to convert to base64.
+            let sri = crypto
+              .createHash('sha512')
+              .update(downloadedData)
+              .digest()
+              .toString('base64');
 
             // Expand template, replacing $url with the source and $sri with the hash value.
             let template = value
